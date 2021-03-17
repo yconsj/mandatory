b22172beaa77af2e1f3a0b3e97ca3e4cbdabe0ef
Peetzie <s174159@student.dtu.dk>
2021-03-17 10:55:07 +0100

    Code cleanup and added pathway for peetz

--- Mandatory/GCL.fsx
+++ Mandatory/GCL.fsx
@@ -1,4 +1,5 @@
-#r "C:/Users/simon/.nuget/packages/fslexyacc.runtime/10.0.0/lib/net46/FsLexYacc.Runtime.dll"
+//#r "C:/Users/simon/.nuget/packages/fslexyacc.runtime/10.0.0/lib/net46/FsLexYacc.Runtime.dll"
+#r "C:/Users/Peetz/.nuget/packages/fslexyacc.runtime/10.0.0/lib/net46/FsLexYacc.Runtime.dll"
 open FSharp.Text.Lexing
 open System
 #load "GCLTypesAST.fs"
@@ -13,43 +14,43 @@ open System.Text;
 //prettyprinter
 let rec prettyPrinterC gcl =
         match gcl with
-        | AssignExpr(a,e)            ->  string "(" + a+ ":=" + (prettyPrinterA e) + ")"
-        | ArrayAssignExpr(a,e1,e2)   ->  string "(" + a + "[" + (prettyPrinterA e1) + "]" + ":=" + (prettyPrinterA e2) + ")"
-        | SemiColonExpr(e1,e2)       ->  string "(" + (prettyPrinterC e1) + ";" + (prettyPrinterC e2) + ")"
-        | IfExpr(e)                  ->  string "(if" + (prettyPrinterGC e) + "fi)"
-        | DoExpr(e)                  ->  string "(do" + (prettyPrinterGC e) + "od)"
-        | SkipExpr(s)                ->  string s
+        | AssignExpr(a,e)                           ->  string "(" + a+ ":=" + (prettyPrinterA e) + ")"
+        | ArrayAssignExpr(a,e1,e2)                  ->  string "(" + a + "[" + (prettyPrinterA e1) + "]" + ":=" + (prettyPrinterA e2) + ")"
+        | SemiColonExpr(e1,e2)                      ->  string "(" + (prettyPrinterC e1) + ";" + (prettyPrinterC e2) + ")"
+        | IfExpr(e)                                 ->  string "(if" + (prettyPrinterGC e) + "fi)"
+        | DoExpr(e)                                 ->  string "(do" + (prettyPrinterGC e) + "od)"
+        | SkipExpr(s)                               ->  string s
 and prettyPrinterGC gcl =
     match gcl with
-      | GCLoopExpr(e1, e2)         -> string "(" + (prettyPrinterGC e1) + "[]" + (prettyPrinterGC e2) + ")"
-      | ArrowExpr(b,e)             -> string "(" + (prettyPrinterB b) + "->" + (prettyPrinterC e) + ")"
+      | GCLoopExpr(e1, e2)                          ->  string "(" + (prettyPrinterGC e1) + "[]" + (prettyPrinterGC e2) + ")"
+      | ArrowExpr(b,e)                              ->  string "(" + (prettyPrinterB b) + "->" + (prettyPrinterC e) + ")"
 and prettyPrinterB gcl =
     match gcl with
     | Bool(x) -> string x
-    | EqualExpr(l,r)      ->  string "("+(prettyPrinterA l) + "=" + (prettyPrinterA r) + ")"
-    | NotEqualExpr(l,r)   ->  string "("+(prettyPrinterA l) + "!=" + (prettyPrinterA r) + ")"
-    | GreaterExpr(l,r)   ->  string "("+(prettyPrinterA l) + ">" + (prettyPrinterA r) + ")"
-    | SmallerExpr(l,r)   ->  string "("+(prettyPrinterA l) + "<" + (prettyPrinterA r) + ")"
-    | EqGreaterExpr(l,r)   ->  string "("+(prettyPrinterA l) + ">=" + (prettyPrinterA r) + ")"
-    | EqSmallerExpr(l,r)   ->  string "("+(prettyPrinterA l) + "<=" + (prettyPrinterA r) + ")"
-    | OrExpr(l,r)       ->  string "("+(prettyPrinterB l) + "|" + (prettyPrinterB r) + ")"
-    | AndExpr(l,r)       ->  string "("+(prettyPrinterB l) + "&" + (prettyPrinterB r) + ")"    
-    | CondOrExpr(l,r)       ->  string "("+(prettyPrinterB l) + "||" + (prettyPrinterB r) + ")"
-    | CondAndExpr(l,r)       ->  string "("+(prettyPrinterB l) + "&&" + (prettyPrinterB r) + ")"    
-    | NotExpr(l)       ->  string "(!" + (prettyPrinterB l) + ")"
+    | EqualExpr(l,r)                                ->  string "("+(prettyPrinterA l) + "=" + (prettyPrinterA r) + ")"
+    | NotEqualExpr(l,r)                             ->  string "("+(prettyPrinterA l) + "!=" + (prettyPrinterA r) + ")"
+    | GreaterExpr(l,r)                              ->  string "("+(prettyPrinterA l) + ">" + (prettyPrinterA r) + ")"
+    | SmallerExpr(l,r)                              ->  string "("+(prettyPrinterA l) + "<" + (prettyPrinterA r) + ")"
+    | EqGreaterExpr(l,r)                            ->  string "("+(prettyPrinterA l) + ">=" + (prettyPrinterA r) + ")"
+    | EqSmallerExpr(l,r)                            ->  string "("+(prettyPrinterA l) + "<=" + (prettyPrinterA r) + ")"
+    | OrExpr(l,r)                                   ->  string "("+(prettyPrinterB l) + "|" + (prettyPrinterB r) + ")"
+    | AndExpr(l,r)                                  ->  string "("+(prettyPrinterB l) + "&" + (prettyPrinterB r) + ")"    
+    | CondOrExpr(l,r)                               ->  string "("+(prettyPrinterB l) + "||" + (prettyPrinterB r) + ")"
+    | CondAndExpr(l,r)                              ->  string "("+(prettyPrinterB l) + "&&" + (prettyPrinterB r) + ")"    
+    | NotExpr(l)                                    ->  string "(!" + (prettyPrinterB l) + ")"
 and prettyPrinterA gcl =
     match gcl with
-    | Num(x) ->  string x
-    | Var(x) -> string x
-    | ArrayExpr(A,x) -> string "(" + A+"[" + (prettyPrinterA x) + "])"
-    | UPlusExpr(l)      ->  string "("+"" + (prettyPrinterA l) + ")"
-    | UMinusExpr(l)     ->  string "(-" + (prettyPrinterA l) + ")"
-    | TimesExpr(l, r)    ->  string  "("+(prettyPrinterA l) + "*" + (prettyPrinterA r) + ")"
-    | DivExpr(l,r)        ->  string "("+(prettyPrinterA l) + "/" + (prettyPrinterA r) + ")"
-    | PlusExpr(l,r)       ->  string "("+(prettyPrinterA l) + "+" + (prettyPrinterA r) + ")"
-    | MinusExpr(l,r)      ->  string "("+(prettyPrinterA l) + "-" + (prettyPrinterA r) + ")"
-    | PowExpr(l,r)        ->  string "("+(prettyPrinterA l) + "^" + (prettyPrinterA r) + ")"
-    | ModExpr(l,r)        ->  string "("+(prettyPrinterA l) + "%" + (prettyPrinterA r) + ")"
+    | Num(x)                                        ->  string x
+    | Var(x)                                        ->  string x
+    | ArrayExpr(A,x)                                ->  string "(" + A+"[" + (prettyPrinterA x) + "])"
+    | UPlusExpr(l)                                  ->  string "("+"" + (prettyPrinterA l) + ")"
+    | UMinusExpr(l)                                 ->  string "(-" + (prettyPrinterA l) + ")"
+    | TimesExpr(l, r)                               ->  string  "("+(prettyPrinterA l) + "*" + (prettyPrinterA r) + ")"
+    | DivExpr(l,r)                                  ->  string "("+(prettyPrinterA l) + "/" + (prettyPrinterA r) + ")"
+    | PlusExpr(l,r)                                 ->  string "("+(prettyPrinterA l) + "+" + (prettyPrinterA r) + ")"
+    | MinusExpr(l,r)                                ->  string "("+(prettyPrinterA l) + "-" + (prettyPrinterA r) + ")"
+    | PowExpr(l,r)                                  ->  string "("+(prettyPrinterA l) + "^" + (prettyPrinterA r) + ")"
+    | ModExpr(l,r)                                  ->  string "("+(prettyPrinterA l) + "%" + (prettyPrinterA r) + ")"
 
 type Edge =
      | Edge of string
@@ -59,33 +60,33 @@ type Node =
 
 let rec compilerC AST n1 n2  count =
         match AST with
-        | AssignExpr(a,e)            ->  [Node(n1, a + ":=" + prettyPrinterA e, n2)]
-        | SemiColonExpr(e1,e2)       ->  let nNew = Edge ("n" + (string (count+1)))
-                                         (compilerC e1 n1 nNew (count+1)) @ (compilerC e2 nNew n2 (count+2))
-        | ArrayAssignExpr(a,e1,e2)   ->  [Node(n1, a + "["+prettyPrinterA e1 + "]:=" + prettyPrinterA e2, n2)]
-        | IfExpr(e)                  ->  compilerGCDET e n1 n2 count ""
-        | DoExpr(e)                  ->  [Node(n1,(computeB e) +"| false)" ,n2)]@(compilerGCDET e n1 n1 count "") //todo fix
-        | SkipExpr(s)                ->  [Node(n1, "skip",n2)]
+        | AssignExpr(a,e)                           ->  [Node(n1, a + ":=" + prettyPrinterA e, n2)]
+        | SemiColonExpr(e1,e2)                      ->  let nNew = Edge ("n" + (string (count+1)))
+                                                        (compilerC e1 n1 nNew (count+1)) @ (compilerC e2 nNew n2 (count+2))
+        | ArrayAssignExpr(a,e1,e2)                  ->  [Node(n1, a + "["+prettyPrinterA e1 + "]:=" + prettyPrinterA e2, n2)]
+        | IfExpr(e)                                 ->  compilerGCDET e n1 n2 count ""
+        | DoExpr(e)                                 ->  [Node(n1,(computeB e) + "| false)" ,n2)]@(compilerGCDET e n1 n1 count "") //todo fix
+        | SkipExpr(s)                               ->  [Node(n1, "skip",n2)]
 and compilerGC AST n1 n2 count =
     match AST with
-    | ArrowExpr(b,e) -> let nNew = Edge ("q" + (string (count+1)))
-                        [Node(n1, (prettyPrinterB b),nNew)] @ (compilerC e nNew n2 (count+1))
-    | GCLoopExpr(e1,e2) -> (compilerGC e1 n1 n2 count) @ (compilerGC e2 n1 n2 (count+1))
+    | ArrowExpr(b,e)                                -> let nNew = Edge ("q" + (string (count+1)))
+                                                       [Node(n1, (prettyPrinterB b),nNew)] @ (compilerC e nNew n2 (count+1))
+    | GCLoopExpr(e1,e2)                             -> (compilerGC e1 n1 n2 count) @ (compilerGC e2 n1 n2 (count+1))
 and compilerGCDET AST n1 n2 count boolx =
     match AST with
-    | ArrowExpr(b,e) -> let nNew = Edge ("q" + (string (count+1)))
-                        match boolx with
-                        | "" -> [Node(n1, (prettyPrinterB b) + "& (!" + boolx + "false)",nNew)] @ (compilerC e nNew n2 (count+1))
-                        | _ -> [Node(n1, (prettyPrinterB b) + "& (!(" + boolx + "false))",nNew)] @ (compilerC e nNew n2 (count+1))           
-    | GCLoopExpr(e1,e2) -> (compilerGCDET e1 n1 n2 count boolx) @ (compilerGCDET e2 n1 n2 (count+1) (computeNonDet e1 + "|" + boolx))   
+    | ArrowExpr(b,e)                                -> let nNew = Edge ("q" + (string (count+1)))
+                                                       match boolx with
+                                                       | "" -> [Node(n1, (prettyPrinterB b) + "& (!" + boolx + "false)",nNew)] @ (compilerC e nNew n2 (count+1))
+                                                       | _  -> [Node(n1, (prettyPrinterB b) + "& (!(" + boolx + "false))",nNew)] @ (compilerC e nNew n2 (count+1))           
+    | GCLoopExpr(e1,e2)                             -> (compilerGCDET e1 n1 n2 count boolx) @ (compilerGCDET e2 n1 n2 (count+1) (computeNonDet e1 + "|" + boolx))   
 and computeB AST =
     match AST with
-    | ArrowExpr(b,e) -> "!" + prettyPrinterB b
-    | GCLoopExpr(e1,e2) -> "(" + (computeB e1) + ")&("+ (computeB e2) + ")"
+    | ArrowExpr(b,e)                                -> "!" + prettyPrinterB b
+    | GCLoopExpr(e1,e2)                             -> "(" + (computeB e1) + ")&("+ (computeB e2) + ")"
 and computeNonDet AST =
     match AST with
-    | ArrowExpr(b,e) -> prettyPrinterB b
-    | GCLoopExpr(e1,e2) -> "(" + (computeNonDet e1) + ")|("+ (computeNonDet e2) + ")"
+    | ArrowExpr(b,e)                                -> prettyPrinterB b
+    | GCLoopExpr(e1,e2)                             -> "(" + (computeNonDet e1) + ")|("+ (computeNonDet e2) + ")"
 
 
 // Pase from calculator example, using to parse readline
@@ -99,8 +100,8 @@ let parse input =
 
 let rec graphString list =
     match list with
-    | (Node(Edge(n1),action,Edge(n2)))::t -> string (n1 + "->" + n2 + "[label= \"" + action + "\"]") + graphString t
-    | [] -> ""
+    | (Node(Edge(n1),action,Edge(n2)))::t           -> string (n1 + "->" + n2 + "[label= \"" + action + "\"]") + graphString t
+    | []                                            -> ""
 // We implement here the function that interacts with the user
 let rec computeAST n =
    if n = 0 then
